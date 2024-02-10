482633a3cbd688a9594c60067eb05fe57ef74e22
Pyry Sangi <pyry.sangi@helsinki.fi>
2024-02-10 20:55:50 +0200

    Fix handling of arguments containing spaces

--- filter.sh
+++ filter.sh
@@ -5,6 +5,6 @@ output=$2
 
 shift 2
 
-sample_rate=$(soxi -r $input)
+sample_rate=$(soxi -r "$input")
 
-sox $input -c 1 -t f32 - | ./a.out "$@" | sox -c 1 -t f32 -r $sample_rate - $output
+sox "$input" -c 1 -t f32 - | ./a.out "$@" | sox -c 1 -t f32 -r $sample_rate - "$output"
