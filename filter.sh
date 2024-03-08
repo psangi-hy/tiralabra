5c4e2d9c9913d82d66788def6fb42bf50732370f
Pyry Sangi <pyry.sangi@helsinki.fi>
2024-03-08 15:52:21 +0200

    Tell the user that their variable sample rate sound file won't work

--- filter.sh
+++ filter.sh
@@ -7,4 +7,8 @@ shift 2
 
 sample_rate=$(soxi -r "$input")
 
+[ $? != 0 ] && exit 1
+[ -z $sample_rate ] && echo "VIRHE: Tiedostolla $input on vaihteleva tai tuntematon näytetaajuus, \
+eikä ohjelma pysty käsittelemään sitä." && exit 1
+
 sox "$input" -c 1 -t f32 - | ./a.out "$@" | sox -c 1 -t f32 -r $sample_rate - "$output"
